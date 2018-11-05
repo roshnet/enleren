7302d2537c6e270a3f864598597103846b346c36
Roshan Sharma <reprogram46@gmail.com>
2018-11-05 12:49:52 +0530

    Database queried (SQLIA vulnerable)
    Use MySQL with Flask to query the DB as in PHP

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask import request
 from flask import redirect
 from flask import flash
 from flask import url_for
-
-# from flask_sqlalchemy import SQLAlchemy
+from flask import session
+from flaskext.mysql import MySQL
 
 # CUSTOM MODULES
 from extras.greetings import greeting
@@ -14,11 +14,15 @@ from extras.usercreds import usercreds
 from utils import auth
 from utils.validate import validate
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'i4d1fr15s8a14'
-# app.config['curr_uname'] = None
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'aaaaa'
+app.config['MYSQL_DATABASE_DB'] = 'enleren'
 
+mysql = MySQL()
+mysql.init_app(app)
 
 @app.route('/')
 @app.route('/index')
@@ -31,20 +35,19 @@ def login():
     # Login page is rendered when method == GET;
     # Form data is processed when method == POST;
     # Login page with a flashed msg is rendered in case of an error.
-
     if request.method == 'POST':
         username = request.form['username']
         passwd = request.form['passwd']
-        response = auth.authorise(username, passwd, usercreds)
-        # Refer docstrings for utils/auth.py to know about `response` values.
-        if response == 1:
-            # app.config['curr_uname'] = username
-            return redirect(url_for('members'), code=302)
-        elif response == 0:
-            flash('Incorrect password. Try again.', 'Problem')
-            return render_template('login.html', prev_username=username)
-        flash('No user named {}. Try again.'.format(username), 'Problem')
-        return render_template('login.html')
+        # add an `if` to abort query execution if non-allowed chars present,
+        # thus preventing SQLIA.
+        cursor = mysql.connect().cursor()
+        cursor.execute("SELECT id FROM usercreds WHERE username='{}' AND password='{}'".format(
+                                                                         username, passwd))
+        data = cursor.fetchone()
+        if data is None:
+            return 'Incorrect pair!'
+        return 'Logged in successfully!'
+
     return render_template('login.html')
 
 
@@ -53,16 +56,18 @@ def signup():
     # Signup page is rendered when method == GET;
     # Form data is processed when method == POST;
     # Signup page with flashed message(s) is rendered if input is invalid.
+    if request.method == 'POST':
+        name = request.form['name']
+        username = request.form['username']
+        passwd = request.form['passwd']
+        response = validate(name, username, passwd)
+        if response == 1:
+            return redirect(url_for('members'), code=302)
+        flash('Some fields too short!')
+        return render_template('signup.html')
+
     if request.method == 'GET':
         return render_template('signup.html')
-    name = request.form['name']
-    username = request.form['username']
-    passwd = request.form['passwd']
-    response = validate(name, username, passwd)
-    if response == 1:
-        return redirect(url_for('members'), code=302)
-    flash('Some fields too short!')
-    return render_template('signup.html')
 
 
 # ending function
